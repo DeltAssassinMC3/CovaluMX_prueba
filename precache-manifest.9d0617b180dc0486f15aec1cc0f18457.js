110f1e7bd0e86150dc10a128454445c6de89b1ba
Julio Rosales <bunbuher95@gmail.com>
2020-06-25 09:39:13 -0500

    Updates

---
+++ precache-manifest.9d0617b180dc0486f15aec1cc0f18457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4024c845df105544728f8cc515c500fa",
+    "url": "/CovaluMX_prueba/index.html"
+  },
+  {
+    "revision": "9f162427d0690389a941",
+    "url": "/CovaluMX_prueba/static/css/main.71c3b908.chunk.css"
+  },
+  {
+    "revision": "92c5474ea5e58d2b6486",
+    "url": "/CovaluMX_prueba/static/js/2.9074da1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CovaluMX_prueba/static/js/2.9074da1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f162427d0690389a941",
+    "url": "/CovaluMX_prueba/static/js/main.ef15c1f2.chunk.js"
+  },
+  {
+    "revision": "31095c79c7db31d5e4e0",
+    "url": "/CovaluMX_prueba/static/js/runtime-main.08cf79ce.js"
+  }
+]);
